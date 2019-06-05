c67622a9025dfd8588401e7cfa6b12432afd2466
Shefass <shefass@gmail.com>
2019-06-05 18:32:43 +0300

    shefas soliution

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 # API Project: Timestamp Microservice for FCC
-
+#My first api!!!
+https://leather-gram.glitch.me
 ### User stories :
 
 1. The API endpoint is `GET [project_url]/api/timestamp/:date_string?`
@@ -11,9 +12,6 @@
 e.g. `{"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}`.
 5. If the date string is **invalid** the api returns a JSON having the structure `{"unix": null, "utc" : "Invalid Date" }`. It is what you get from the date manipulation functions used above.
 
-#### Example usage:
-* https://curse-arrow.hyperdev.space/api/timestamp/2015-12-15
-* https://curse-arrow.hyperdev.space/api/timestamp/1450137600000
 
 #### Example output:
 * { "unix": 1450137600, "natural": "December 15, 2015" }
--- server.js
+++ server.js
@@ -2,31 +2,49 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+//My first API
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string", function(req, res) {
+  var date = req.params.date_string;
+  
+  if (/\D/.test(date)) {   //user enters only numbers
+    if (/\d\-\d{2}\-\d{2}$/.test(date)){
+      var dateInstance = new Date(date);
+      res.json({ unix: dateInstance.getTime(), utc: dateInstance.toUTCString()});
+    } else {
+       res.json({"unix": null, "utc" : "Invalid Date" })
+    }
+   
+  } else {
+    var dateInstance = new Date(Number(date));
+    res.json({ unix: dateInstance.getTime(), utc: dateInstance.toUTCString()});
+   
+  }
 });
 
-
+//if user dont enters
+app.get("/api/timestamp/", function(req, res) {
+  var dateInstance = new Date();
+  res.json({ unix: dateInstance.getTime(), utc: dateInstance.toUTCString() });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
